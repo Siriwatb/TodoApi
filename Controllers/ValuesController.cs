7452364b6e611b740ff1511ac5d66463181b499b
Siriwat.B <Siriwatb@users.noreply.github.com>
2019-11-21 22:13:21 +0700

    Initial commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//using Values.Models;
+
+namespace Values.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public ValuesController()
+        {
+        }
+
+        // GET api/values
+        [HttpGet("")]
+        public ActionResult<IEnumerable<string>> Getstrings()
+        {
+            return new List<string> { };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> GetstringById(int id)
+        {
+            return null;
+        }
+
+        // POST api/values
+        [HttpPost("")]
+        public void Poststring(string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Putstring(int id, string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void DeletestringById(int id)
+        {
+        }
+    }
+}
